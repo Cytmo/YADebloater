3cb4994b84f1442ceb2fd1cbf1e9cddaca80fc2e
cytmo <cytmo@qq.com>
2023-01-31 14:14:37 +0800

    fix. modified gzip's c file to let pycparser parse it

---
+++ python_deb/debloater/cparser.py
@@ -0,0 +1,44 @@
+# # parser_file 用于处理c语言文件
+# from pycparser import parse_file
+# from pycparser import CParser
+# # c语言有错误时，会引出此错误
+# from pycparser.plyparser import ParseError
+# # c_ast.py 文件下包含了抽象语法树的节点类
+# from pycparser.c_ast import *
+
+
+# c_file = open('../gzip-1.2.4/gzip-1.2.4.c.origin.c', 'r')
+# ast = parse_file('../gzip-1.2.4/gzip-1.2.4.c.origin.c', use_cpp = True,cpp_path=)
+
+
+# print(ast.ext[0])
+#-------------------------------------------------------------------------------
+# pycparser: using_gcc_E_libc.py
+#
+# Similar to the using_cpp_libc.py example, but uses 'gcc -E' instead
+# of 'cpp'. The same can be achieved with Clang instead of gcc. If you have
+# Clang installed, simply replace 'gcc' with 'clang' here.
+#
+# Eli Bendersky [https://eli.thegreenplace.net/]
+# License: BSD
+#-------------------------------------------------------------------------------
+import sys
+
+# This is not required if you've installed pycparser into
+# your site-packages/ with setup.py
+#
+sys.path.extend(['.', '..'])
+
+from pycparser import parse_file
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        filename  = sys.argv[1]
+    else:
+        filename = '../gzip-1.2.4/gzip-1.2.4.c.origin.c'
+
+    ast = parse_file(filename, use_cpp=True,
+            cpp_path='gcc',
+            cpp_args=['-E', r'-Iutils/fake_libc_include'])
+    ast.show()
