c2542c30236372f6c830d4862d06ad98b311ddc4
cytmo <cytmo@qq.com>
2023-02-17 14:03:26 +0800

    feat. use logger to print info & add a full successful debloat process - small_progs/replace

--- python_deb/debloater/temp/run.py
+++ python_deb/debloater/temp/run.py
@@ -8,7 +8,7 @@ source_path=''
 
 def compile_with_cov(source,dest=""):
     print('Compiling to '+source+"_origin")
-
+    # gcc will cause the program to seg fault, use clang instead
     abspath = os.path.abspath(source)
     ret = subprocess.call(["gcc",abspath,"-w", "-o",abspath+"_origin","-fprofile-arcs","-ftest-coverage"])
     if(utils.exit_status(ret,"Compile")==0):
@@ -38,7 +38,7 @@ def debloat():
     
 def verify():
     run_tests("tmp.log2")
-    cmd = 'diff tmp.log tmp.log2'
+    cmd = 'diff temp/tmp.log temp/tmp.log2'
     ret = execute(cmd)    
     if(ret==0):
         print("Verify successed!")
@@ -46,11 +46,43 @@ def verify():
         print("Verify failed!")
     
 def run_tests(output_file="tmp.log"):
+    cmds = []
+    current_work_dir = os.path.dirname(__file__)
+    output_file = current_work_dir + os.sep + output_file
 
-    for fname in os.listdir('temp/train'):
-        fpath = os.path.join('temp/train', fname)
-        # -c
-        cmd = BIN + ' -c < ' + fpath + ' > '+output_file
+    # $BIN '[d]*' '&@t& lkj[0-9]&' < $INDIR/moni/rr19.t &>> $OUTDIR/o0
+    cmd0 = "{} '[d]*' '&@t& lkj[0-9]&' < {}/train/moni/rr19.t >> {}".format(BIN, current_work_dir, output_file)
+    # $BIN '@h' 'w0Yj|`l`AYgv{M<#KAouk' < $INDIR/input/ruin.1035 &>> $OUTDIR/o1    
+    cmd1 = "{} '@h' 'w0Yj|`l`AYgv{{M<#KAouk' < {}/train/input/ruin.1035 >> {}".format(BIN, current_work_dir, output_file)
+    # $BIN '@**?' 'b@t' < $INDIR/temp-test/2179.inp.922.1 &>> $OUTDIR/o2
+    cmd2 = "{} '@**?' 'b@t' < {}/train/temp-test/2179.inp.922.1 >> {}".format(BIN, current_work_dir, output_file)
+    # $BIN '?[^9-B]' 'a@n' < $INDIR/temp-test/282.inp.126.2 &>> $OUTDIR/o3
+    cmd3 = "{} '?[^9-B]' 'a@n' < {}/train/temp-test/282.inp.126.2 >> {}".format(BIN, current_work_dir, output_file)
+    # $BIN '-[^-z]' '&a@%' < $INDIR/temp-test/768.inp.329.1 &>> $OUTDIR/o4
+    cmd4 = "{} '-[^-z]' '&a@%' < {}/train/temp-test/768.inp.329.1 >> {}".format(BIN, current_work_dir, output_file)
+    # $BIN 'x' 'vif#21aJuxd&,I1PPnf{b,On|j&db8)b<`\|' < $INDIR/input/ruin.334 &>> $OUTDIR/o5
+    cmd5 = "{} 'x' 'vif#21aJuxd&,I1PPnf{{b,On|j&db8)b<`\|' < {}/train/input/ruin.334 >> {}".format(BIN, current_work_dir, output_file)
+    #  $BIN '%yw,0+8@n' 'a&`' < $INDIR/input/ruin.1158 &>> $OUTDIR/o6
+    cmd6 = "{} '%yw,0+8@n' 'a&`' < {}/train/input/ruin.1158 >> {}".format(BIN, current_work_dir, output_file)
+    # $BIN '%?[@@][^0-9][a-c]?-[^0-9]- [^9-B]$' 'NEW' < $INDIR/temp-test/1129.inp.485.10 &>> $OUTDIR/o7
+    cmd7 = "{} '%?[@@][^0-9][a-c]?-[^0-9]- [^9-B]$' 'NEW' < {}/train/temp-test/1129.inp.485.10 >> {}".format(BIN, current_work_dir, output_file)
+    # $BIN '-?@@*[_-z][^0-9]' '&' < $INDIR/temp-test/821.inp.354.1 &>> $OUTDIR/o8
+    cmd8 = "{} '-?@@*[_-z][^0-9]' '&' < {}/train/temp-test/821.inp.354.1 >> {}".format(BIN, current_work_dir, output_file)
+    # $BIN '[^9-B][^]@[**' 'a' < $INDIR/moni/f7.inp &>> $OUTDIR/o9
+    cmd9 = "{} '[^9-B][^]@[**' 'a' < {}/train/moni/f7.inp >> {}".format(BIN, current_work_dir, output_file)
+    
+    cmds.append(cmd0)
+    cmds.append(cmd1)
+    cmds.append(cmd2)
+    cmds.append(cmd3)
+    cmds.append(cmd4)
+    cmds.append(cmd5)
+    cmds.append(cmd6)
+    cmds.append(cmd7)
+    cmds.append(cmd8)
+    cmds.append(cmd9)
+    
+    for cmd in cmds:
         execute(cmd)
     
 
