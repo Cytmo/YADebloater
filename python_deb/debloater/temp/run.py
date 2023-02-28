eca0e6dd4debc268108a8cdfae68c620981ac63a
cytmo <cytmo@qq.com>
2023-02-28 14:27:34 +0800

    feat&fix. fix a few bugs and support input fuzz

--- python_deb/debloater/temp/run.py
+++ python_deb/debloater/temp/run.py
@@ -4,13 +4,13 @@ import os, subprocess, sys
 import utils
 BIN = ''
 source_path=''
-logger = ''
+logger=''
 
 def compile_with_cov(source,dest=""):
     logger.info('Compiling to '+source+"_origin")
-    # gcc will cause the program to seg fault, use clang instead
+
     abspath = os.path.abspath(source)
-    ret = subprocess.call(["gcc",abspath,"-w", "-o",abspath+"_origin","-fprofile-arcs","-ftest-coverage","-D __msan_unpoison(s,z)","-lpcre"])    
+    ret = subprocess.call(["gcc",abspath,"-w", "-o",abspath+"_origin","-fprofile-arcs","-ftest-coverage"])
     if(utils.exit_status(ret,"Compile")==0):
         utils.move_file("*.gcno","temp")
         global BIN 
@@ -20,9 +20,9 @@ def compile_with_cov(source,dest=""):
 
 
 def execute(cmd):
-    logger.info('running '+ cmd)
+    logger.info('running '+cmd)
     p = subprocess.Popen(cmd, shell=True)
-    p.communicate(timeout=5)
+    p.communicate()
     return p.returncode
 
 def debloat():
@@ -38,46 +38,29 @@ def debloat():
     
 def verify():
     run_tests("tmp.log2")
-    cmd = 'diff temp/tmp.log temp/tmp.log2'
+    cmd = 'diff tmp.log tmp.log2'
     ret = execute(cmd)    
     if(ret==0):
-        print("Verify successed!")
+        logger.info("Verify successed!")
     else:
-        print("Verify failed!")
+        logger.info("Verify failed!")
     
-    
-def begin_run(arg):
-    cmd = BIN + ' ' + arg
-    execute(cmd)
-
 def run_tests(output_file="tmp.log"):
-    cmds = []
-    current_work_dir = os.path.dirname(__file__)
-    output_file = current_work_dir + os.sep + output_file
-    begin_run( """ "a" temp/train1  >> {}""".format(output_file))
-    begin_run( """ -n "si" temp/train1  >> {}""".format(output_file))
-    begin_run( """ -o [r][a][n][d]* temp/train1  >> {}""".format(output_file))
-    begin_run( """ -v "a" temp/train1  >> {}""".format(output_file))
-    begin_run( """ -i "Si" temp/train1  >> {}""".format(output_file))
-    begin_run( """ -w "Si" temp/train1  >> {}""".format(output_file))
-    begin_run( """ -x "Don't" temp/train1  >> {}""".format(output_file))
-    begin_run( """ -E "randomtext*" temp/train1  >> {}""".format(output_file))
-    begin_run( """ "ye " temp/train1  >> {}""".format(output_file))
-    begin_run( """ "cold" temp/train1  >> {}""".format(output_file))
-    begin_run( """ "not exist" temp/train1  >> {}""".format(output_file))
-    begin_run( """ ^D  temp/train1  >> {}""".format(output_file))
-    begin_run( """ .$  temp/train1  >> {}""".format(output_file))
-    begin_run( """ \^  temp/train1  >> {}""".format(output_file))
-    begin_run( """ \^$  temp/train1  >> {}""".format(output_file))
-    begin_run( """ ^[AEIOU]  temp/train1  >> {}""".format(output_file))
-    begin_run( """ ^[^AEIOU]  temp/train1  >> {}""".format(output_file))
-    begin_run( """ -E "free[^[:space:]]+"  temp/train1  >> {}""".format(output_file))
-    begin_run( """ -E '\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?\.)3}}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'  temp/train1  >> {}""".format(output_file))
-    # cmds.append(cmd9)
+    if output_file != 'tmp.log2':
+        for fname in os.listdir('temp/train'):
+            for i in range(0,10):
+                logger.debug('bbbbbbb')
+                fpath = os.path.join('temp/train', fname)
+                cmd = 'radamsa --seed {} {} > {}_rad_{}'.format(i,fpath, fpath,i)
+                logger.info('Generating fuzzed testfiles, cmd is {}'.format(cmd))
+                execute(cmd)
+    for fname in os.listdir('temp/train'):
+        fpath = os.path.join('temp/train', fname)
+        # -c
+        cmd = BIN + ' -c < ' + fpath + ' > '+output_file
+        execute(cmd)
     
-    # for cmd in cmds:
-    #     execute(cmd)
-    return
+
 
 def test():
     BIN = './gzip.orig_temp/gzip.orig.debloated'
@@ -85,15 +68,13 @@ def test():
     for fname in os.listdir('test'):
         fpath = os.path.join('test', fname)
         # -c
-        cmd = BIN + ' -c < ' + fpath + ' >> tmp.log'
+        cmd = BIN + ' -c < ' + fpath + ' > tmp.log'
         execute(cmd)
 
 
 def clean():
     for fname in os.listdir('./'):
         if fname == "run.py" or fname == "utils.py":
-            continue
-        if fname.startswith('test') or fname.startswith('train') or fname == "backup":
             continue
         
         if fname == 'test' or fname == 'train' or fname == "backup":
@@ -105,13 +86,12 @@ def clean():
         execute('rm -rf ./' + fname)
 
 def usage():
-    print('python run.py clean|run_tests|debloat\n')
+    logger.info('python run.py clean|run_tests|debloat\n')
     sys.exit(1)
 
 def main():
     global BIN,logger,source_path
     logger = utils.GetLog().get_log()
-    
 
     if len(sys.argv) != 2 and len(sys.argv) != 3:
         usage()
